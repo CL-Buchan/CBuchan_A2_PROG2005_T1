a34a1088ed187e10faa5963996cc61b90ceb997a
CL-Buchan <clathanbuchan@gmail.com>
2026-03-21 10:02:21 +1000

    fixed: server refactor to render other file types

--- Part_A/app/server/server.js
+++ Part_A/app/server/server.js
@@ -9,13 +9,12 @@ const port = 8080;
 const server = createServer((req, res) => {
 	// Gets the current path of the URL after the first '/'
 	let pathname = url.parse(req.url).pathname;
-	pathname = '.' + pathname;
+	let filePath;
 
-	if (pathname === './') {
-		pathname = path.join(__dirname, '../pages/index.html');
-	} else {
-		pathname = path.join(pathname, '/pages/index.html');
-	}
+	// Gets the suffix of the path eg. '.html' or '.css' etc
+	const ext = path.extname(pathname);
+
+	pathname = '.' + pathname;
 
 	// Will implement more methods as the project goes on
 	if (req.method !== 'GET') {
@@ -24,24 +23,34 @@ const server = createServer((req, res) => {
 		return res.end('Other methods are not yet implemented.');
 	}
 
+	// Checks to see if the path is empty and the URL is pointing to the root
+	if (pathname === '/' || ext === '') {
+		filePath = path.join(__dirname, '../pages', pathname, 'index.html');
+	} else {
+		filePath = path.join(__dirname, '..', pathname);
+	}
+
+	// Store possible MIME types (file types)
+	const mimeTypes = {
+		'.html': 'text/html',
+		'.css': 'text/css',
+		'.js': 'text/javascript',
+	};
+
+	// mimeTypes[ext] gets the value from the key value pair 
+	const contentType = mimeTypes[ext] || 'text/plain';
+
 	// Reads the file and writes/sends the data to the file (either HTML or CSS)
-	fs.readFile(pathname, (error, data) => {
+	fs.readFile(filePath, 'utf-8', (error, data) => {
 		if (error) {
 			res.statusCode = 404;
 			res.setHeader('Content-Type', 'text/plain');
-			return res.end(`An error has occurred: ${error}`);
+			return res.end(`File not found: ${error.message}`);
 		}
 
-		if (pathname.endsWith('.css')) {
-			res.statusCode = 200;
-			res.setHeader('Content-Type', 'text/css');
-		} else {
-			res.statusCode = 200;
-			res.setHeader('Content-Type', 'text/html');
-		}
-
-		res.write(data);
-		res.end();
+		res.statusCode = 200;
+		res.setHeader('Content-Type', contentType);
+		res.end(data);
 	});
 });
 
