b39443a436e89294403810d2ac82b7f978447433
CL-Buchan <clathanbuchan@gmail.com>
2026-03-29 21:40:11 +1000

    added: app components and defined route structure

--- Part_B/src/app/app.routes.ts
+++ Part_B/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ManageComponent } from './pages/manage/manage.component';
+import { SearchComponent } from './pages/search/search.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
+import { HelpComponent } from './pages/help/help.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'manage',
+    component: ManageComponent,
+  },
+  {
+    path: 'search',
+    component: SearchComponent,
+  },
+  {
+    path: 'privacy',
+    component: PrivacyComponent,
+  },
+  {
+    path: 'help',
+    component: HelpComponent,
+  },
+];
